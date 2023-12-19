c45ef2efc3b9e37129539e2109f34c0ff0383d52
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2023-12-19 14:26:32 +0100

    fixed panier bugs

--- views/shop.php
+++ views/shop.php
@@ -427,11 +427,9 @@ if (empty($_SESSION['client'])) {
         <a class="navbar-brand text-white" href="#">
             OPEP
         </a>
-
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
